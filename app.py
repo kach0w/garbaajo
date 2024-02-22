619cd2eb816f7bca07a248c21bd9f3f9d226cc81
kach0w <karsab343@gmail.com>
2024-02-21 16:25:00 -0800

    d

--- app.py
+++ app.py
@@ -12,40 +12,14 @@ def load_model():
     model.load_weights("weights.h5")
     return model
 def preprocess_image(image):
-    # Resize the image to match the input shape of your model
     resized_image = cv.resize(image, (100, 100))
-    # Normalize the image pixel values
     normalized_image = resized_image / 255.0
-    # Add batch dimension
     preprocessed_image = np.expand_dims(normalized_image, axis=0)
     return preprocessed_image
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    st.error("Unable to open camera.")
-
-while cap.isOpened():
-    ret, frame = cap.read()
-
-    if not ret:
-        st.error("Error reading frame.")
-        break
-
-    # Process the captured frame
+frame = st.camera_input("Take Your Photo");
+if frame is not None:
     processed_frame = preprocess_image(frame)
-
-    # Perform prediction
     prediction = model.predict(processed_frame)
-
-    # Display prediction or any other output
-    st.write("Prediction:", prediction)  # Modify this to display your prediction
-
-    # Display the captured frame
+    st.write("Prediction:", prediction) 
     st.image(frame, channels="BGR")
-
-    # Close the camera when the "Close Camera" button is clicked
-    if st.button("Close Camera"):
-        break
-
-    # Release the camera
-cap.release()
