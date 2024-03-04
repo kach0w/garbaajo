e6571db7f77ec4254f9e7c69033ca9e6928f0e8b
kach0w <karsab343@gmail.com>
2024-03-03 22:06:22 -0800

    adf

--- app.py
+++ app.py
@@ -5,21 +5,6 @@ from PIL import Image
 import torch
 
 st.title("Garbage Classification App")
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5) # 100 - (5-1) /2 => 48
-        self.conv2 = nn.Conv2d(6, 8, 5) # 48 - 4 / 2 = 22
-        self.fc1 = nn.Linear(8*22*22, 12)
-        # self.fc2 = nn.Linear(12, 12) 
-
-    def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2)) 
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
-        x = x.view(-1, 8*22*22)
-        x = self.fc1(x)
-        # x = self.fc2(x)
-        return x
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = torch.load("model.pth")
